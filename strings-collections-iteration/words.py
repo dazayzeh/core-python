ff584299a88304deb860c47ed362df5b52ef8ad6
Dania Azayzeh <onlydaniaaz@gmail.com>
2020-07-29 12:07:21 +0200

    separate main fun

--- strings-collections-iteration/words.py
+++ strings-collections-iteration/words.py
@@ -11,10 +11,18 @@ def fetch_words():
             story_words.append(word)
 
     story.close()
+    return story_words
 
+
+def print_words(story_words):
     for word in story_words:
         print(word)
 
 
+def main():
+    words = fetch_words()
+    print_words(words)
+
+
 if __name__ == '__main__':
-    fetch_words()
+    main()

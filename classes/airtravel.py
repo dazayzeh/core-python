f3e37f53a881778aa0931d47650e0ab2b0d45cf7
Dania Azayzeh <onlydaniaaz@gmail.com>
2020-07-30 15:25:48 +0200

    create invarients in class flight to  verify the flight number

--- classes/airtravel.py
+++ classes/airtravel.py
@@ -4,6 +4,12 @@
 class Flight:
 
     def __init(self, number):
+        if not number[:2].isalpha():
+            raise ValueError(f'first two digits should contain an airline code {number}')
+        if not number[2:].isdigit():
+            raise ValueError(f'invalid rout number {number}')
+        if not number[:2].isupper():
+            raise ValueError(f'invalid code {number}')
         self._number = number
 
     def number(self):

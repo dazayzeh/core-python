001a789d014eccc6401b3ecbfc6cb7581ee75191
Dania Azayzeh <onlydaniaaz@gmail.com>
2020-07-30 17:00:50 +0200

    create relocate functionality

--- classes/airtravel.py
+++ classes/airtravel.py
@@ -17,6 +17,11 @@ class Flight:
         self._seating = [None] + [ { l : None for l in seats} for _ in rows]   # throw first row because it starts at zero
 
     def allocate_seat(self, seat, customer_name):
+        """Locate a customer in a seat if available
+
+        :param seat: the chosen seat
+        :param customer_name: the customer name
+        """
         row, seat_letter = self._parse_seat(seat)
 
         if self._seating[row][seat_letter] is not None:
@@ -24,7 +29,29 @@ class Flight:
 
         self._seating[row][seat_letter] = customer_name
 
+    def relocate_seat(self, from_seat, to_seat):
+        """Relocate customers to a different seat
+
+        :param from_seat: the current occupied seat
+        :param to_seat: the new seat
+        """
+        from_row, from_seat_letter = self._parse_seat(from_seat)
+        if self._seating[from_row][from_seat_letter] is None:
+            raise ValueError(f'no customer to relocate in seat {from_seat}')
+
+        to_row, to_seat_letter = self._parse_seat(to_seat)
+        if self._seating[to_row][to_seat_letter] is not None:
+            raise ValueError(f'seat {to_seat} is already taken')
+
+        self._seating[to_row][to_seat_letter] = self._seating[from_row][from_seat_letter]
+        self._seating[from_row][from_seat_letter] = None
+
     def _parse_seat(self, seat):
+        """ check if a given seat exists
+
+        :param seat: the desired seat to parse
+        :return: a tuple of the seat row and letter
+        """
         rows, seats_letter = self._aircraft.seating_plan()
 
         seat_letter = seat[-1]
@@ -40,7 +67,7 @@ class Flight:
     def number_of_available_seats(self):
         return sum(   # get the sum of all sums
             sum(1 for seat in row.values() if seat is None)  # generator expression to add 1 for each available row
-                   for row in self._seatingn  # access each row in the list
+                   for row in self._seating  # access each row in the list
                    if row is not None  # if not first row because we defined it as None
         )
 
